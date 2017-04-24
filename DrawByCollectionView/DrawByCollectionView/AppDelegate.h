d72abdcc15cffc04637c5a8c66b1f2329bdc1d83
宋永建 <ldusyj@163.com>
2017-04-24 18:48:35 +0800

    new project

---
+++ DrawByCollectionView/DrawByCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawByCollectionView
+//
+//  Created by 宋永建 on 2017/4/24.
+//  Copyright © 2017年 宋永建. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawByCollectionView/DrawByCollectionView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DrawByCollectionView
+//
+//  Created by 宋永建 on 2017/4/24.
+//  Copyright © 2017年 宋永建. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
