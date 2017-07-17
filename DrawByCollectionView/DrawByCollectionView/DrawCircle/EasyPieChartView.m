0e221463007c37889e5e61090d82b71f03a81d5c
宋永建 <ldusyj@163.com>
2017-07-17 16:53:06 +0800

    测试 数据 方法

--- DrawByCollectionView/DrawByCollectionView/DrawCircle/EasyPieChartView.m
+++ DrawByCollectionView/DrawByCollectionView/DrawCircle/EasyPieChartView.m
@@ -97,9 +97,9 @@
 }
 
 - (void)extractPieChartModelList{
+    [self.pieChartModelList removeAllObjects];
     
-    CGFloat startAngle = 0;//- 2 * M_PI * arr[0];
-    
+    CGFloat startAngle = 0;
     for (int i = 0 ; i < [self.percentList count]; i++) {
         DrawPieChartModel *model = [DrawPieChartModel circleModelWithSrartAngle:startAngle
                                                                      arcPercent:self.percentList[i]
@@ -123,7 +123,6 @@
             (fabs(model.textLeftCenterPoint.y - lastModel.textLeftCenterPoint.y) < 15)) {
             [model resetLinePositionWithLastPointY:lastModel.textLeftCenterPoint.y];
         }
-        
         lastModel = model;
     }
 }
@@ -132,68 +131,6 @@
     [self.pieChartView resetCircleList:self.pieChartModelList];
     [self.pieChartLineView resetLine:self.pieChartModelList];
 }
-
-//测试代码
-//1、拿到数据
-//2、检查数据的合理性
-//3、整合创建model
-//4、检查是否存在极端情况
-- (void)test {
-    
-    NSArray *colorArray = @[[UIColor colorWithRed:(64)/255.0 green:(186)/255.0 blue:(255)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(255)/255.0 green:(133)/255.0 blue:(155)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(255)/255.0 green:(179)/255.0 blue:(0)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(152)/255.0 green:(230)/255.0 blue:(123)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(64)/255.0 green:(186)/255.0 blue:(255)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(255)/255.0 green:(133)/255.0 blue:(155)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(255)/255.0 green:(179)/255.0 blue:(0)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(152)/255.0 green:(230)/255.0 blue:(123)/255.0 alpha:1.0]];
-    
-    NSArray *arcTextArray  = @[@"精通",@"薄弱",@"了解",@"掌握",@"精通",@"薄弱",@"了解",@"掌握"];
-    
-    NSMutableArray *circleModelList = [NSMutableArray array];
-    
-    float arr[] = {0.2,0.2,0.01,0.01,0.18,0.1,0.2,0.1};
-    
-//    float tArr = arr[0] + arr[1] + arr[2];
-//    
-//    arr[3] = 1 - tArr;
-    //    NSAssert(tArr <= 1, @"测试数组百分比 和 大于 1");
-    
-    CGFloat startAngle = 0;//- 2 * M_PI * arr[0];
-    
-//    for (int i = 0 ; i < 8; i++) {
-//        DrawPieChartModel *model = [DrawPieChartModel circleModelWithSrartAngle:startAngle
-//                                                                     arcPercent:arr[i]
-//                                                                          color:colorArray[i]
-//                                                                        arcText:arcTextArray[i]
-//                                                                      arcCenter:self.arcCenter];
-//        startAngle = model.endAngle;
-//        [circleModelList addObject:model];
-//    }
-//    
-    DrawPieChartModel *lastModel = nil;
-    
-    for (DrawPieChartModel *model in circleModelList) {
-        if (lastModel == nil) {
-            lastModel = [circleModelList lastObject];
-        }
-        
-        if (model.arcCenterQuadrant == lastModel.arcCenterQuadrant &&
-            (fabs(model.textLeftCenterPoint.y - lastModel.textLeftCenterPoint.y) < 15)) {
-            [model resetLinePositionWithLastPointY:lastModel.textLeftCenterPoint.y];
-        }
-        
-        lastModel = model;
-    }
-    
-    
-    
-    
-    [self.pieChartView resetCircleList:circleModelList];
-    [self.pieChartLineView resetLine:circleModelList];
-}
-
 
 #pragma mark - LazyLoads
 - (NSArray *)colorList {
--- DrawByCollectionView/DrawByCollectionView/DrawCircleViewController.m
+++ DrawByCollectionView/DrawByCollectionView/DrawCircleViewController.m
@@ -19,6 +19,11 @@
 #import "EasyPieChartView.h"
 
 #define STDScreenW [UIScreen mainScreen].bounds.size.width
+
+#define random(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
+
+#define randomColor random(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256))
+
 @interface DrawCircleViewController ()
 
 @property (nonatomic, strong) DrawPieChartView *circleView;
@@ -27,6 +32,8 @@
 @property (nonatomic, copy) NSArray<NSNumber *> *percentList;
 @property (nonatomic, copy) NSArray<UIColor *> *colorList;
 @property (nonatomic, copy) NSArray<NSString *> *arcTextList;
+
+@property (nonatomic, strong) EasyPieChartView *pieView;
 
 @end
 
@@ -37,6 +44,7 @@
     [super viewDidLoad];
     [self p_configOwnProperties];
     [self p_configSubViews];
+    [self setupTestButton];
 }
 
 - (void)dealloc {
@@ -49,12 +57,12 @@
 }
 
 - (void)p_configSubViews {
-    EasyPieChartView *view = [EasyPieChartView pieChartViewWithFrame:CGRectMake(0, 100, STDScreenW, 200)];
-    [self.view addSubview:view];
-    
-    [view drawPieChartWithPercentList:self.percentList
-                            colorList:self.colorList
-                          arcTextList:self.arcTextList];
+    EasyPieChartView *pieView = [EasyPieChartView pieChartViewWithFrame:CGRectMake(0, 100, STDScreenW, 200)];
+    [self.view addSubview:pieView];
+    self.pieView = pieView;
+    [pieView drawPieChartWithPercentList:self.percentList
+                               colorList:self.colorList
+                             arcTextList:self.arcTextList];
 }
 
 - (NSArray<NSNumber *> *)percentList {
@@ -81,258 +89,63 @@
     return _arcTextList;
 }
 
-/*
- 
- NSArray *colorArray = @[[UIColor colorWithRed:(64)/255.0 green:(186)/255.0 blue:(255)/255.0 alpha:1.0],
- [UIColor colorWithRed:(255)/255.0 green:(133)/255.0 blue:(155)/255.0 alpha:1.0],
- [UIColor colorWithRed:(255)/255.0 green:(179)/255.0 blue:(0)/255.0 alpha:1.0],
- [UIColor colorWithRed:(152)/255.0 green:(230)/255.0 blue:(123)/255.0 alpha:1.0],
- [UIColor colorWithRed:(64)/255.0 green:(186)/255.0 blue:(255)/255.0 alpha:1.0],
- [UIColor colorWithRed:(255)/255.0 green:(133)/255.0 blue:(155)/255.0 alpha:1.0],
- [UIColor colorWithRed:(255)/255.0 green:(179)/255.0 blue:(0)/255.0 alpha:1.0],
- [UIColor colorWithRed:(152)/255.0 green:(230)/255.0 blue:(123)/255.0 alpha:1.0]];
- 
- NSArray *arcTextArray  = @[@"精通",@"薄弱",@"了解",@"掌握",@"精通",@"薄弱",@"了解",@"掌握"];
- 
- NSMutableArray *circleModelList = [NSMutableArray array];
- 
- float arr[] = {0.2,0.2,0.01,0.01,0.18,0.1,0.2,0.1};
- 
- */
-
-/*
-#pragma mark - HandleViews
-- (void)p_configSubViews {
-    [self setupDrawCircleView];
-    [self setupDrawCircleLineView];
-    [self test];
+- (void)setupTestButton {
+    CGFloat width = 80;
+    CGFloat height = 40;
+    CGFloat x = (STDScreenW - width) * 0.5;
+    CGFloat y = CGRectGetMaxY(self.pieView.frame) + 180;
+    UIButton *testButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [testButton setTitle:@"点击测试" forState:UIControlStateNormal];
+    [testButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    testButton.frame = CGRectMake(x, y, width, height);
+    [testButton addTarget:self
+                   action:@selector(testButtonClicked:)
+         forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:testButton];
 }
 
-- (void)setupDrawCircleView {
-    DrawPieChartView *circleView = [[DrawPieChartView alloc] init];
-    circleView.frame = CGRectMake(0, 240, STDScreenW, 200);
-    [self.view addSubview:circleView];
-    self.circleView = circleView;
+- (void)testButtonClicked:(UIButton *)button {
+    [self randomNumList];
+    [self randomColorList];
+    [self randomText];
     
+    [self.pieView drawPieChartWithPercentList:self.percentList
+                                    colorList:self.colorList
+                                  arcTextList:self.arcTextList];
 }
 
-- (void)setupDrawCircleLineView {
-    DrawPieChartLineView *lineView = [[DrawPieChartLineView alloc] init];
-    lineView.frame = CGRectMake(0, 240, STDScreenW, 200);
-    [self.view addSubview:lineView];
-    self.lineView = lineView;
+- (void)randomNumList {
+    NSMutableArray *numMutableList = [NSMutableArray array];
+    int sum = 0;
+    while (YES) {
+        int temp = arc4random_uniform(60) + 1;
+        if (sum + temp >= 100) {
+            [numMutableList addObject:@((100 - sum) / 100.0)];
+            break;
+        } else {
+            [numMutableList addObject:@(temp / 100.0)];
+            sum += temp;
+        }
+    }
+    self.percentList = numMutableList;
 }
 
-- (void)test {
-    
-    NSArray *colorArray = @[[UIColor colorWithRed:(64)/255.0 green:(186)/255.0 blue:(255)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(255)/255.0 green:(133)/255.0 blue:(155)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(255)/255.0 green:(179)/255.0 blue:(0)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(152)/255.0 green:(230)/255.0 blue:(123)/255.0 alpha:1.0]];
-    
-    NSArray *arcTextArray  = @[@"精通",@"薄弱",@"了解",@"掌握"];
-    
-    NSMutableArray *circleModelList = [NSMutableArray array];
-    
-    float arr[] = {0.2,0.2,0.31,0.2};
-    
-    float tArr = arr[0] + arr[1] + arr[2];
-    
-    arr[3] = 1 - tArr;
-//    NSAssert(tArr <= 1, @"测试数组百分比 和 大于 1");
-    
-    CGFloat startAngle = 0;//- 2 * M_PI * arr[0];
-    
-    for (int i = 0 ; i < 2; i++) {
-        DrawPieChartModel *model = [DrawPieChartModel circleModelWithSrartAngle:startAngle
-                                                                   arcPercent:arr[i]
-                                                                      color:colorArray[i]
-                                                                    arcText:arcTextArray[i]];
-        startAngle = model.endAngle;
-        [circleModelList addObject:model];
+- (void) randomColorList {
+    NSMutableArray *colorMutableList = [NSMutableArray array];
+    for (int i = 0; i < [self.percentList count]; i++) {
+        [colorMutableList addObject:randomColor];
     }
     
-    DrawPieChartModel *lastModel = nil;
-    
-    for (DrawPieChartModel *model in circleModelList) {
-        if (lastModel == nil) {
-            lastModel = [circleModelList lastObject];
-        }
-        
-        if (model.arcCenterQuadrant == lastModel.arcCenterQuadrant &&
-            (fabs(model.textLeftCenterPoint.y - lastModel.textLeftCenterPoint.y) < 15)) {
-            [model resetLinePositionWithLastPointY:lastModel.textLeftCenterPoint.y];
-        }
-        
-        lastModel = model;
-    }
-    
-    
-    
-
-    [self.circleView resetCircleList:circleModelList];
-    [self.lineView resetLine:circleModelList];
+    self.colorList = colorMutableList;
 }
 
-
-- (void)test2 {
-    
-    NSArray *colorArray = @[[UIColor colorWithRed:(64)/255.0 green:(186)/255.0 blue:(255)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(255)/255.0 green:(133)/255.0 blue:(155)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(255)/255.0 green:(179)/255.0 blue:(0)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(152)/255.0 green:(230)/255.0 blue:(123)/255.0 alpha:1.0]];
-    
-    NSArray *arcTextArray  = @[@"精通",@"薄弱",@"了解",@"掌握"];
-    
-    NSMutableArray *circleModelList = [NSMutableArray array];
-    
-    float arr[] = {0.21,0.3,0.11,0.2};
-    
-    float tArr = arr[0] + arr[1] + arr[2];
-    
-    arr[3] = 1 - tArr;
-    NSAssert(tArr <= 1, @"测试数组百分比 和 大于 1");
-    
-    CGFloat startAngle = 0;//- 2 * M_PI * arr[0];
-    
-    for (int i = 0 ; i < 4; i++) {
-        DrawPieChartModel *model = [DrawPieChartModel circleModelWithSrartAngle:startAngle
-                                                                 arcPercent:arr[i]
-                                                                      color:colorArray[i]
-                                                                    arcText:arcTextArray[i]];
-        startAngle = model.endAngle;
-        [circleModelList addObject:model];
+- (void)randomText {
+    NSMutableArray *textMutableList = [NSMutableArray array];
+    NSArray *textList = @[@"精通",@"薄弱",@"了解",@"掌握",@"skillful",@"weak",@"know",@"master"];
+    for (int i = 0; i < [self.percentList count]; i++) {
+        [textMutableList addObject:textList[i]];
     }
-    
-    DrawPieChartModel *lastModel = nil;
-    
-    for (DrawPieChartModel *model in circleModelList) {
-        if (lastModel == nil) {
-            lastModel = [circleModelList lastObject];
-        }
-        
-        if (model.arcCenterQuadrant == lastModel.arcCenterQuadrant &&
-            (fabs(model.textLeftCenterPoint.y - lastModel.textLeftCenterPoint.y) < 15)) {
-            [model resetLinePositionWithLastPointY:lastModel.textLeftCenterPoint.y];
-        }
-        
-        lastModel = model;
-    }
-    
-    
-    
-    
-    [self.circleView resetCircleList:circleModelList];
-    [self.lineView resetLine:circleModelList];
+    self.arcTextList = textMutableList;
 }
 
-
-
-
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        [self testAll];
-    });
-    
-}
-
-- (void)testAll {
-    
-    dispatch_async(dispatch_get_global_queue(0, NO), ^{
-        NSMutableArray *array = [NSMutableArray array];
-        for (int i = 56; i <= 100; i ++) {
-            for (int j = 0; j <= 100; j ++) {
-                for (int k = 0; k <= 100; k++) {
-                    if (i + j + k <= 100) {
-                        int m = 100 - (i + j + k);
-                        if (i > 0) {
-                            [array addObject:@(i)];
-                        }
-                        if (j > 0) {
-                            [array addObject:@(j)];
-                        }
-                        if (k > 0) {
-                            [array addObject:@(k)];
-                        }
-                        if (m > 0) {
-                            [array addObject:@(m)];
-                        }
-                        
-                        
-                        dispatch_async(dispatch_get_main_queue(),^{
-                            
-                            //想要在主线程中执行的代码 如刷新UI
-                            [self testt:array];
-                            
-                        });
-                        
-                        [NSThread sleepForTimeInterval:.1];
-                        [array removeAllObjects];
-                    }
-                }
-            }
-        }
-    });
-    
-   
-}
-
-
-
-
-- (void)testt:(NSMutableArray *)muArr {
-    
-    
-    
-    NSArray *colorArray = @[[UIColor colorWithRed:(64)/255.0 green:(186)/255.0 blue:(255)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(255)/255.0 green:(133)/255.0 blue:(155)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(255)/255.0 green:(179)/255.0 blue:(0)/255.0 alpha:1.0],
-                            [UIColor colorWithRed:(152)/255.0 green:(230)/255.0 blue:(123)/255.0 alpha:1.0]];
-    
-    NSArray *arcTextArray  = @[@"精通",@"薄弱",@"了解",@"掌握"];
-    
-    NSMutableArray *circleModelList = [NSMutableArray array];
-    
-    float arr[] = {0.01,0.97,0.01,0.2};
-    
-    for (int i = 0; i < [muArr count]; i ++) {
-        arr[i] = [muArr[i] intValue] / 100.0;
-    }
-    
-//    NSAssert(tArr <= 1, @"测试数组百分比 和 大于 1");
-    
-    CGFloat startAngle = 0;//- 2 * M_PI * arr[0];
-    
-    for (int i = 0 ; i < [muArr count]; i++) {
-        DrawPieChartModel *model = [DrawPieChartModel circleModelWithSrartAngle:startAngle
-                                                                 arcPercent:arr[i]
-                                                                      color:colorArray[i]
-                                                                    arcText:arcTextArray[i]];
-        startAngle = model.endAngle;
-        [circleModelList addObject:model];
-    }
-    
-    DrawPieChartModel *lastModel = nil;
-    
-    for (DrawPieChartModel *model in circleModelList) {
-        if (lastModel == nil) {
-            lastModel = [circleModelList lastObject];
-        }
-        
-        if (model.arcCenterQuadrant == lastModel.arcCenterQuadrant &&
-            (fabs(model.textLeftCenterPoint.y - lastModel.textLeftCenterPoint.y) < 15)) {
-            [model resetLinePositionWithLastPointY:lastModel.textLeftCenterPoint.y];
-        }
-        
-        lastModel = model;
-    }
-    
-    
-    
-    
-    [self.circleView resetCircleList:circleModelList];
-    [self.lineView resetLine:circleModelList];
-}*/
 @end
