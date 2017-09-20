216c8707aa6080dcc0d8f264b279578b681c4c7e
宋永建 <ldusyj@163.com>
2017-09-20 15:29:27 +0800

    icon

--- EasyChartsSwift/EasyChartsSwift/ViewController.swift
+++ EasyChartsSwift/EasyChartsSwift/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        self.title = "ECS"
     }
 
     override func didReceiveMemoryWarning() {
