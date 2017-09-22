343a1e11faa2ce3554fb292c743c95f43dea954a
宋永建 <ldusyj@163.com>
2017-09-22 10:20:53 +0800

    add ECBrokenLineNormalCollectionView

---
+++ Examples/EasyCharts/BrokenLineChart/ECBrokenLineCollectionView.swift
@@ -0,0 +1,13 @@
+//
+//  ECBrokenLineCollectionView.swift
+//  Examples
+//
+//  Created by 宋永建 on 2017/9/22.
+//  Copyright © 2017年 Global. All rights reserved.
+//
+
+import UIKit
+
+class ECBrokenLineCollectionView: UICollectionView {
+
+}
